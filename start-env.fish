47fd412a494bb8cebb7e4394f7441ceb3f4c2431
BingyangWu <wf201267@pku.edu.cn>
2024-03-29 11:45:32 +0800

    Add a script for starting the environment on Shanghai AI Lab's machine

---
+++ start-env.fish
@@ -0,0 +1,4 @@
+conda activate vllm
+export CUDA_HOME=/mnt/petrelfs/share/cuda-12.1/
+export MAX_JOBS=32
+export LD_LIBRARY_PATH=/mnt/petrelfs/share/gcc/mpfr-4.1.0/lib/:/mnt/petrelfs/share/cuda-12.1/lib64/stubs/
